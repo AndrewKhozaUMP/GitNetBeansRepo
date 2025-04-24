d0229609b714a7e6abf981a76e41ece2ad279075
Andrew <201991349@ump.ac.za>
2025-04-24 11:09:08 +0200

    feature branch first commit

---
+++ src/main/java/Login.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Dell
+ */
+public class Login {
+    
+    public static void loginMessage(){
+        System.out.println("Please login");
+    }
+
+}
